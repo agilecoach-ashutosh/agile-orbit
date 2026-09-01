ca992c10f576e4e53cf30bc0195cf1e574418a12
agilecoach-ashutosh <agilecoach.ashutosh@gmail.com>
2026-09-01 14:49:31 +0530

    Add files via upload

---
+++ tools.js
@@ -0,0 +1,2 @@
+
+(function(){document.addEventListener('DOMContentLoaded',()=>{document.querySelectorAll('[data-print]').forEach(b=>b.addEventListener('click',AO.print));document.querySelectorAll('[data-reset]').forEach(b=>b.addEventListener('click',()=>{b.closest('form')?.reset();location.reload()}));});})();
