7bde0edb501270dbbc09fef2557be6b94e09c14c
agilecoach-ashutosh <agilecoach.ashutosh@gmail.com>
2026-09-05 09:53:35 +0530

    Restore and load master practice question bank

--- js/practice-data.js
+++ js/practice-data.js
@@ -1,5 +1,2 @@
-/*
- * Practice question bank intentionally cleared.
- * The previous question bank has been removed. A new clean bank will be added later.
- */
-window.PRACTICE_QUESTIONS = [];
+window.PRACTICE_THEMES="+json.dumps([[k,themes[k]] for k in themes],ensure_ascii=False,separators=(',',':'))+";
+window.PRACTICE_QUESTIONS="+json.dumps(records,ensure_ascii=False,separators=(',',':'))+";
